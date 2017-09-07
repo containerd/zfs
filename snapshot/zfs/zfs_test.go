d83bc500d3cf7ddfbfe6c1ea8b56dde38c388ea8
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2017-09-07 05:26:55 +0000

    Update containerd (v1.0.0-beta.0)
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>

--- snapshot/zfs/zfs_test.go
+++ snapshot/zfs/zfs_test.go
@@ -15,55 +15,62 @@ import (
 	"github.com/containerd/containerd/snapshot/testsuite"
 	"github.com/containerd/containerd/testutil"
 	zfs "github.com/mistifyio/go-zfs"
+	"github.com/pkg/errors"
 )
 
-func newTestZpool(t *testing.T) (string, func()) {
-	lo, destroyLo := testutil.NewLoopback(t, 1<<30) // 1GiB
+func newTestZpool() (string, func() error, error) {
+	lo, destroyLo, err := testutil.NewLoopback(1 << 30) // 1GiB
+	if err != nil {
+		return "", nil, err
+	}
 	zpoolName := fmt.Sprintf("testzpool-%d", time.Now().UnixNano())
 	zpool, err := zfs.CreateZpool(zpoolName, nil, lo)
 	if err != nil {
-		t.Fatal(err)
+		return "", nil, err
 	}
-	return zpoolName, func() {
+	return zpoolName, func() error {
 		if err := zpool.Destroy(); err != nil {
-			t.Fatal(err)
+			return err
 		}
-		destroyLo()
-	}
+		return destroyLo()
+	}, nil
 }
 
-func newSnapshotter(t *testing.T) func(context.Context, string) (snapshot.Snapshotter, func(), error) {
-	return func(ctx context.Context, root string) (snapshot.Snapshotter, func(), error) {
-		testZpool, destroyTestZpool := newTestZpool(t)
+func newSnapshotter() func(context.Context, string) (snapshot.Snapshotter, func() error, error) {
+	return func(ctx context.Context, root string) (snapshot.Snapshotter, func() error, error) {
+		testZpool, destroyTestZpool, err := newTestZpool()
+		if err != nil {
+			return nil, nil, err
+		}
 		testZFSMountpoint, err := ioutil.TempDir("", "containerd-zfs-test")
 		if err != nil {
-			t.Fatal(err)
+			return nil, nil, err
 		}
 		testZFSName := filepath.Join(testZpool, "containerd-zfs-test")
 		testZFS, err := zfs.CreateFilesystem(testZFSName, map[string]string{
 			"mountpoint": testZFSMountpoint,
 		})
 		if err != nil {
-			t.Fatalf("could not create zfs %q on %q: %v", testZFSName, testZFSMountpoint, err)
+			return nil, nil, errors.Wrapf(err, "could not create zfs %q on %q", testZFSName, testZFSMountpoint)
 		}
 		snapshotter, err := NewSnapshotter(testZFSMountpoint)
 		if err != nil {
-			t.Fatal(err)
+			return nil, nil, err
 		}
 
-		return snapshotter, func() {
+		return snapshotter, func() error {
 			if err := testZFS.Destroy(zfs.DestroyRecursive | zfs.DestroyRecursiveClones | zfs.DestroyForceUmount); err != nil {
-				t.Fatal(err)
+				return err
 			}
 			if err := os.RemoveAll(testZFSMountpoint); err != nil {
-				t.Fatal(err)
+				return err
 			}
-			destroyTestZpool()
+			return destroyTestZpool()
 		}, nil
 	}
 }
 
 func TestZFS(t *testing.T) {
 	testutil.RequiresRoot(t)
-	testsuite.SnapshotterSuite(t, "zfs", newSnapshotter(t))
+	testsuite.SnapshotterSuite(t, "zfs", newSnapshotter())
 }
